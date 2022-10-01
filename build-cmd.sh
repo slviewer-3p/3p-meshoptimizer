3f6ccd3a26e66af30c336b1df5a612f75f2a57e0
Nicky Dasmijn <nicky.dasmijn@posteo.nl>
2022-10-01 11:57:24 +0200

    Replace -m$AUTOBUILD_ADDRSIZE with $AUTOBUILD_GCC_ARCH

--- build-cmd.sh
+++ build-cmd.sh
@@ -79,7 +79,7 @@ pushd "$MESHOPT_SOURCE_DIR"
         ;;
 
         linux*)
-            opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
+            opts="${TARGET_OPTS:-${AUTOBUILD_GCC_ARCH} $LL_BUILD_RELEASE}"
 
             # Handle any deliberate platform targeting
             if [ -z "${TARGET_CPPFLAGS:-}" ]; then
